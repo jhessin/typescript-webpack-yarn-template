2deadc9bbe8da788a937b6a4a179df6a0852c15a
Jim Hessin <jhessin@gmail.com>
2021-05-15 15:50:19 -0600

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+const config = {
+  entry: './src/index.ts',
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    compress: true,
+    host: '0.0.0.0',
+    watchContentBase: true,
+    overlay: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      }
+    ]
+  },
+  resolve: {
+    extensions: [
+      '.tsx',
+      '.ts',
+      '.js',
+    ],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  }
+}
+
+module.exports = config;
